7fbac4e2c0d64361dc6a4229c6a11cf39d5d0cd9
achmadmulyadi <achmadmulyadi@gmail.com>
2023-08-10 09:13:14 +0700

    add hashutil

--- utils/hashutil.js
+++ utils/hashutil.js
@@ -1,39 +1,24 @@
-import { scrypt, randomBytes, timingSafeEqual } from "crypto";
+const crypto = require("crypto")
 
-const keyLength = 32;
-/**
- * Has a password or a secret with a password hashing algorithm (scrypt)
- * @param {string} password
- * @returns {string} The salt+hash
- */
-export const hash = async (password) => {
+async function hash(password) {
     return new Promise((resolve, reject) => {
-        // generate random 16 bytes long salt - recommended by NodeJS Docs
-        const salt = randomBytes(16).toString("hex");
+        const salt = crypto.randomBytes(8).toString("hex")
 
-        scrypt(password, salt, keyLength, (err, derivedKey) => {
+        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
             if (err) reject(err);
-            // derivedKey is of type Buffer
-            resolve(`${salt}.${derivedKey.toString("hex")}`);
+            resolve(salt + ":" + derivedKey.toString('hex'))
         });
-    });
-};
+    })
+}
 
-/**
- * Compare a plain text password with a salt+hash password
- * @param {string} password The plain text password
- * @param {string} hash The hash+salt to check against
- * @returns {boolean}
- */
-export const compare = async (password, hash) => {
+async function verify(password, hash) {
     return new Promise((resolve, reject) => {
-        const [salt, hashKey] = hash.split(".");
-        // we need to pass buffer values to timingSafeEqual
-        const hashKeyBuff = Buffer.from(hashKey, "hex");
-        scrypt(password, salt, keyLength, (err, derivedKey) => {
+        const [salt, key] = hash.split(":")
+        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
             if (err) reject(err);
-            // compare the new supplied password with the hashed password using timeSafeEqual
-            resolve(timingSafeEqual(hashKeyBuff, derivedKey));
+            resolve(key == derivedKey.toString('hex'))
         });
-    });
-};
+    })
+}
+
+module.exports = {hash, verify}
